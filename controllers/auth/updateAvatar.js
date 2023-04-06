5a867f665059f36d95091556388c1dc4fccaf87c
Lawyer0901 <lawyer0901@gmail.com>
2023-04-06 14:59:30 +0300

    made jimp need to fix postman read files

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res, next) => {
@@ -8,6 +9,14 @@ const updateAvatar = async (req, res, next) => {
   const { _id: id } = req.user;
   try {
     const resultUpload = path.join(avatarsDir, `${id}_${originalname}`);
+    const img = await jimp.read(resultUpload);
+    await img
+      .autocrop(
+        20,
+        20,
+        jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(resultUpload);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", `${id}_${originalname}`);
     const avatar = await User.findByIdAndUpdate(req.user._id, { avatarURL });
