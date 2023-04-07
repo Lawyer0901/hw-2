4f47219bdc02cde5ecd02875d554aba7a0ad7895
Lawyer0901 <lawyer0901@gmail.com>
2023-04-07 15:33:20 +0300

    fixed public

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,27 +1,35 @@
 const { User } = require("../../models");
 const path = require("path");
-const fs = require("fs");
-const jimp = require("jimp");
+const fs = require("fs/promises");
+// const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
 const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
+  console.log(tempUpload);
+
   const { _id: id } = req.user;
+  // console.log(req.user);
   try {
     const resultUpload = path.join(avatarsDir, `${id}_${originalname}`);
-    const img = await jimp.read(resultUpload);
-    await img
-      .autocrop(
-        20,
-        20,
-        jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
-      )
-      .writeAsync(resultUpload);
+
+    // const img = await jimp.read(resultUpload);
+
+    // await img
+    //   .autocrop(
+    //     20,
+    //     20,
+    //     jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
+    //   )
+    //   .writeAsync(resultUpload);
+
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", `${id}_${originalname}`);
-    const avatar = await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    // console.log("test 3", renameAndRemoveToPublic);
+    const avatarURL = path.join("avatars", `${id}_${originalname}`);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.status(200).json({
-      avatarURL: avatar,
+      avatarURL,
     });
   } catch (error) {
     await fs.unlink(tempUpload);
